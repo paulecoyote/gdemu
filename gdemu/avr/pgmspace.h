b0708389c2af062d526a2b5ceb9972d7257aa4ab
kaetemi <none@none>
2011-06-10 14:41:10 +0200

    Added: pgm_read_word

--- gdemu/avr/pgmspace.h
+++ gdemu/avr/pgmspace.h
@@ -34,8 +34,9 @@
 
 #define PROGMEM
 #define pgm_read_byte_near *
-#define pgm_read_byte *
+#define pgm_read_byte pgm_read_byte_near
 inline unsigned short pgm_read_word_near(void *ptr) { return *((short *)ptr); }
+#define pgm_read_word pgm_read_word_near
 typedef signed char prog_char;
 typedef unsigned char prog_uchar;
 
